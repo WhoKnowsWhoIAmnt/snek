3ce5f8170fa54b15b2c3ffa27c7fef2fcea98fb3
WhoKnowsWhoIAm <154082631+WhoKnowsWhoIAmnt@users.noreply.github.com>
2026-08-17 19:59:29 -0400

    The main file that makes snek snek.

--- snek.hpp
+++ snek.hpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <random>
+#include <cstdlib>
 
 //all the stuff to write "Cobra" code
 #define code_snippet auto
@@ -10,6 +12,7 @@
 #define isnot !=
 #define isgreaterthan >
 #define islessthan <
+#define subtract -=
 #define becomes =
 #define stop break
 #define the (
@@ -27,15 +30,20 @@
 #define gathering struct
 #define words std::string
 #define number int
+#define letter char
+#define decimal double
+#define truth bool
 #define get return
 #define start_code int main() {
 #define actually &
 #define but *
 #define magic ->
 #define nothing nullptr
-#define comment(x)
+#define comment(x) // nothing
 #define plus +
 #define minus -
+#define times *
+#define over /
 #define compare switch
 #define isit case
 #define ifso :
@@ -43,14 +51,33 @@
 #define with ,
 #define name_numbers enum class
 #define list(type) std::vector<type>
+#define redtext "\033[31m"
+#define bluetext "\033[34m"
+#define clearformat "\033[0m"
+#define force_end_code exit(0);
 
 void write(const auto& print) {
     std::cout << print << std::endl;
 }
 
 auto input(const std::string& prompt) {
-    std::cout << prompt;
     std::string input;
-    std::getline(std::cin, input);
-    return input;
+    while (true) {
+        std::cout << prompt << std::endl;
+        if (std::cin >> input) {
+            return input;
+        }
+
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+}
+
+std::random_device rd;
+std::mt19937 gen(rd());
+
+int randomnumber(int min, int max) {
+    std::uniform_int_distribution<> dist(min, max);
+    int randomNum = dist(gen);
+    return randomNum;
 }
